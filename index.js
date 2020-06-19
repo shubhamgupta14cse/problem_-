e4e96898105f4cf7afc5e2294606285160fe1454
Shubham Gupta <shubhamgupta14cse@users.noreply.github.com>
2020-06-19 18:41:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+// iterate through the keys
+// assign an object for the key value and return
+// check if the key already exist retyur an array
+
+//BCR log(n)
+
+// lets do it by brute approach firsrt and later optimise
+
+function mergeArarys(keys, value) {
+  var obj = {};
+  if (keys.length !== value.length) return;
+
+  keys.map(( key, index, keys ) => {
+    if (obj.hasOwnProperty(key) ) {
+      var array = Array.isArray(obj[key])? obj[key]: [obj[key]]
+      array.push(value[index]);
+      obj[key] = array
+    } else {
+      obj[key] = value[index]
+    }
+  })
+
+  return obj
+}
+
+var keys = ['username', 'first-name', 'last-name', 'age', 'username'];
+var values = ['johndoe', 'John', 'Doe', 35, 'johnny']
+console.log("This is the returned value ------->", mergeArarys(keys, values))
